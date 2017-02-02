b35bc1088431ad58ab209d988eb2d189ef4c7605
Roman Stepanov <romik112@yandex.ru>
2017-02-02 19:53:33 +0300

    Добавил обработчики для работы с клавиатурой

--- js/form.js
+++ js/form.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var ENTER_KEY_CODE = 13;
+var ESC_KEY_CODE = 27;
+
+var pinMap = document.querySelector('.tokyo__pin-map');
 var pins = document.querySelectorAll('.pin');
 
 var dialogWindow = document.querySelector('.dialog');
-var dialogClose = dialogWindow.querySelector('.dialog__close');
 
 var noticeTitle = document.querySelector('#title');
 var noticePrice = document.querySelector('#price');
@@ -17,28 +20,75 @@ var noticeCapacity = document.querySelector('#capacity');
 var deactivatePins = function () {
   for (var i = 0; i < pins.length; i++) {
     pins[i].classList.remove('pin--active');
+    pins[i].setAttribute('aria-pressed', 'false');
   }
 };
 
-var hideDialog = function () {
+var closeDialog = function () {
   dialogWindow.classList.add('invisible');
   deactivatePins();
+
+  dialogWindow.removeEventListener('click', closeDialogHandler);
+  dialogWindow.removeEventListener('keydown', keydownDialogHandler);
+  pinMap.removeEventListener('keydown', keydownDialogHandler);
 };
 
 var showDialog = function () {
   dialogWindow.classList.remove('invisible');
+
+  dialogWindow.addEventListener('click', closeDialogHandler);
+  dialogWindow.addEventListener('keydown', keydownDialogHandler);
+  pinMap.addEventListener('keydown', keydownDialogHandler);
 };
 
 var selectPin = function (pin) {
   deactivatePins();
   pin.classList.add('pin--active');
+  pin.setAttribute('aria-pressed', 'true');
   showDialog();
 };
 
-var clickPin = function (pin) {
-  pin.addEventListener('click', function () {
-    selectPin(pin);
-  });
+var selectPinHandler = function (evt) {
+  var target = evt.target;
+
+  while (target !== pinMap) {
+    if (target.classList.contains('pin')) {
+      selectPin(target);
+    }
+    target = target.parentNode;
+  }
+};
+
+var isPressENTER = function (evt) {
+  return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
+};
+
+var isPressESC = function (evt) {
+  return evt.keyCode && evt.keyCode === ESC_KEY_CODE;
+};
+
+var keydownMapHandler = function (evt) {
+  if (isPressENTER(evt)) {
+    selectPinHandler(evt);
+  }
+};
+
+var closeDialogHandler = function (evt) {
+  var target = evt.target;
+
+  while (target !== dialogWindow) {
+    if (target.classList.contains('dialog__close')) {
+      evt.preventDefault();
+      closeDialog();
+    }
+    target = target.parentNode;
+  }
+};
+
+var keydownDialogHandler = function (evt) {
+  if (isPressESC(evt)) {
+    closeDialog();
+  }
 };
 
 var validationForm = function () {
@@ -75,11 +125,8 @@ var changeRoom = function () {
   }
 };
 
-
-for (var i = 0; i < pins.length; i++) {
-  clickPin(pins[i]);
-}
-dialogClose.addEventListener('click', hideDialog);
+pinMap.addEventListener('click', selectPinHandler);
+pinMap.addEventListener('keydown', keydownMapHandler);
 validationForm();
 noticeTime.addEventListener('change', changeTime);
 noticeTimeout.addEventListener('change', changeTimeout);
